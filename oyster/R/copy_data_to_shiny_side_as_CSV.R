559c7c5122a1fdd56b10efa2a4fed66570218546
Art Steinmetz <apsteinmetz@yahoo.com>
2024-06-23 11:56:19 -0400

    finished eda. start working on memo

--- oyster/R/copy_data_to_shiny_side_as_CSV.R
+++ oyster/R/copy_data_to_shiny_side_as_CSV.R
@@ -1,14 +1,13 @@
 # copy data files to shiny app www directory as csv
 library(tidyverse)
+library(arrow)
 datasets <- c("wq_data", "wq_meta", "weather")
 
 copy_to_shiny <- function(name) {
-  load(paste0("data/",name,".rdata"))
-  dframe = eval(parse(text = name))
+  dframe <- arrow::read_parquet(paste0("data/",name,".parquet"))
+  # dframe = eval(parse(text = name))
   fname = paste0("NY_Harbor_WQ/www/", name, ".csv")
-  write_csv(dframe,fname)
+  arrow::write_csv_arrow(dframe, fname)
 }
 
 walk(datasets, copy_to_shiny)
-
-
